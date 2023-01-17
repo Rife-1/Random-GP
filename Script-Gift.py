4fad37eaa2daa19b86118da67838865e608be4e1
Mr. Tutul <106426526+Tutul-King@users.noreply.github.com>
2023-01-18 02:29:52 +0600

    Add files via upload

---
+++ Script-Gift.py
@@ -0,0 +1,11 @@
+import os,time
+print('\033[0;91m\n Checking Your Mobile...\033[1;37m')
+time.sleep(10)
+print('\033[0;92m\n Congratulations! Your Device Support This Tools\033[1;37m')
+time.sleep(5)
+print('\033[0;93m\n JOIN MY SCRIPT GIFT GROUP\033[1;37m')
+time.sleep(3)
+os.system("xdg-open https://facebook.com/groups/554714119911648/")
+import R4NDOM
+R4NDOM.main()
+ 
