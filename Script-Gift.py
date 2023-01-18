a01d436b2194b8bc0b2cfebb832c6979d4a90bc3
Mr. Tutul <106426526+Tutul-King@users.noreply.github.com>
2023-01-18 13:51:23 +0600

    Script-Gift.py

--- Script-Gift.py
+++ Script-Gift.py
@@ -1,11 +1,30 @@
-import os,time
-print('\033[0;91m\n Checking Your Mobile...?\033[1;37m')
+import os, platform, time, sys
+def xoss(z):
+    for e in z + '\n':
+        sys.stdout.write(e)
+        sys.stdout.flush()
+        time.sleep(0.050)
+xoss('\033[0;91m Checking Your Mobile...?');time.sleep(0.50)
 time.sleep(10)
-print('\033[0;92m\n Congratulations! Your Device Support This Tools\033[1;37m')
-time.sleep(5)
-print('\033[0;93m\n JOIN MY SCRIPT GIFT GROUP\033[1;37m')
-time.sleep(3)
-os.system("xdg-open https://facebook.com/groups/554714119911648/")
-import R4NDOM
-R4NDOM.main()
+def Update():
+    exit('\033[1;31m Commands On Update Please Wait For Update')
+def Run():
+        bit = platform.architecture()[0]
+        if bit == '64bit':
+            xoss("\033[0;92m Congratulations Your Device Support this Tools")
+            time.sleep(5)
+            xoss('\033[0;93m JOIN MY SCRIPT GIFT GROUP')
+            time.sleep(3)
+            os.system('xdg-open https://facebook.com/groups/554714119911648/')
+            from R4NDOM import refat
+            refat()
+        elif bit == '32bit':
+            xoss("\033[0;92m Congratulations Your Device Support This Tools")
+            xoss('\033[0;93m JOIN MY SCRIPT GIFT GROUP')
+            os.system('xdg-open https://facebook.com/groups/554714119911648/')
+            from R4NDOM import refat
+            refat()
+        else:
+            exit('\033[1;31m Connection & Network Error')
+Run()
  
